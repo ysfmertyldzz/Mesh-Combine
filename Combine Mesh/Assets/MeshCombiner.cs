f103dcaea71c85121931fa19c16dfcfcaa9807f7
ysfmertyldzz <77334914+ysfmertyldzz@users.noreply.github.com>
2021-07-05 19:27:30 +0300

    Combine Mesh
    Combine Mesh

---
+++ Combine Mesh/Assets/MeshCombiner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour
+{
+    public void CombineMeshes()
+    {
+        Quaternion oldRot = transform.rotation;// -5 5 5 / 10 20 0
+        Vector3 oldPos = transform.position;
+
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        Mesh finalMesh = new Mesh();
+
+        CombineInstance[] combiners = new CombineInstance[meshFilters.Length];// + -
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            if (meshFilters[i].transform == transform)
+                continue;
+
+            combiners[i].subMeshIndex = 0;
+            combiners[i].mesh = meshFilters[i].sharedMesh;
+            combiners[i].transform = meshFilters[i].transform.localToWorldMatrix;
+        }
+
+        finalMesh.CombineMeshes(combiners);
+
+        GetComponent<MeshFilter>().sharedMesh = finalMesh;
+
+        transform.rotation = oldRot;
+        transform.position = oldPos;
+
+        for (int i = 0; i < transform.GetChildCount(); i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Combine Mesh/Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int test;
+
+    public GameObject prefab;
+
+    public Vector2 minPos;
+    public Vector2 maxPos;
+
+    private void Start()
+    {
+        for (int i = 0; i < test; i++)
+        {
+            GameObject obj = Instantiate(prefab,new Vector2(Random.RandomRange(minPos.x, maxPos.x), Random.RandomRange(minPos.y, maxPos.y)),Quaternion.identity,this.transform);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<MeshCombiner>().CombineMeshes();
+
+    }
+}
